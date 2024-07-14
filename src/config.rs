d9a326b903cf7e5e982272f32dd16af2f163958d
Alessandro Pira <alex@alessandropira.org>
2024-07-14 15:56:58 +0200

    graceful shutdown

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,14 @@
 use std::path::Path;
 use std::{fs,error::Error};
 use serde::Deserialize;
+use std::time::Duration;
 use std::net::{ToSocketAddrs, SocketAddr};
 
 #[derive(Deserialize)]
 pub struct Config {
 	remote: String,
 	bind: Option<String>,
+	graceful_shutdown_timeout: Option<String>,
 }
 
 impl Config {
@@ -16,7 +18,7 @@ impl Config {
 		//let cfg = content.parse::<toml::Table>()?;
 		match toml::from_str(&content) {
 			Ok(v) => Ok(v),
-			Err(err) => Err(Box::from(format!("TOML parsing error: {}", err)))
+			Err(err) => Err(Box::from(format!("Config parsing error: {}", err)))
 		}
 	}
 
@@ -29,6 +31,32 @@ impl Config {
 			}
 		}
 		([127, 0, 0, 1], 3000).into()
+	}
+
+	pub fn get_graceful_shutdown_timeout(&self) -> Duration {
+		if let Some(def) = &self.graceful_shutdown_timeout {
+			let mut pars = def.trim().to_lowercase();
+			let mut mult: u64 = 1000;
+			if pars.ends_with("sec") {
+				pars.pop();
+				pars.pop();
+				pars.pop();
+			} else if pars.ends_with("ms") {
+				pars.pop();
+				pars.pop();
+				mult = 1;
+			} else if pars.ends_with("min") {
+				pars.pop();
+				pars.pop();
+				pars.pop();
+				mult = 60000;
+			}
+			let pars = pars.trim().to_string();
+			if let Ok(v) = pars.parse::<u64>() {
+				return Duration::from_millis(v * mult);
+			}
+		}
+		Duration::from_secs(10)
 	}
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ use hyper::service::Service;
 use std::pin::Pin;
 use std::future::Future;
 use hyper_util::rt::tokio::{TokioIo, TokioTimer};
+use tokio::signal::unix::{signal, SignalKind};
+use log::{info,warn,error};
 
 mod config;
-
 
 #[derive(Clone)]
 struct Svc {
@@ -31,13 +32,24 @@ impl Service<Request<Incoming>> for Svc {
 			"/" => mk_response("home!".into()),
 			_ => mk_response("not found".into()),
 		};
-		Box::pin(async { res })
+		Box::pin(async {
+			tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+			res
+		})
 	}
 }
 
+async fn shutdown_signal() {
+	signal(SignalKind::interrupt())
+		.expect("failed to install SIGINT signal handler")
+		.recv()
+		.await;
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+	simple_logger::SimpleLogger::new().init().unwrap();
+
 	let cfg = match config::Config::load("config.toml") {
 		Ok(v) => v,
 		Err(e) => panic!("{}", e)
@@ -46,20 +58,42 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 	let addr = cfg.get_bind();
 	let svc = Svc {};
 
+	let graceful = hyper_util::server::graceful::GracefulShutdown::new();
+	let mut signal = std::pin::pin!(shutdown_signal());
+
 	let listener = TcpListener::bind(addr).await?;
-	println!("Listening on http://{}", addr);
+	info!("Listening on http://{}", addr);
 	loop {
-		let (tcp, _) = listener.accept().await?;
-		let io = TokioIo::new(tcp);
-		let svc_clone = svc.clone();
-		tokio::task::spawn(async move {
-			if let Err(err) = http1::Builder::new()
-				.timer(TokioTimer::new())
-				.serve_connection(io, svc_clone)
-				.await
-			{
-				println!("Error serving connection: {:?}", err);
-			}
-		});
+		tokio::select! {
+			Ok((tcp, _addr)) = listener.accept() => {
+				let io = TokioIo::new(tcp);
+				let svc_clone = svc.clone();
+				let conn = http1::Builder::new()
+						.timer(TokioTimer::new())
+						.serve_connection(io, svc_clone);
+				let fut = graceful.watch(conn);
+				tokio::task::spawn(async move {
+					if let Err(err) = fut.await {
+						error!("Error serving connection: {:?}", err);
+					}
+				});
+			},
+			_ = &mut signal => {
+				info!("graceful shutdown signal received");
+				break;
+			},
+		}
 	}
+
+	tokio::select! {
+		_ = graceful.shutdown() => {
+			info!("all connections gracefully closed");
+		},
+		_ = tokio::time::sleep(cfg.get_graceful_shutdown_timeout()) => {
+			warn!("timed out wait for all connections to close");
+		}
+	}
+
+	Ok(())
 }
+
