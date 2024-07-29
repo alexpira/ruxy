d40d1e6dfbc634d412e57c7f5bffed714f6da07f
Alessandro Pira <alex@alessandropira.org>
2024-07-29 06:51:38 +0200

    ecs logging

--- src/logcfg.rs
+++ src/logcfg.rs
@@ -1,20 +1,59 @@
+use chrono::{Local, Utc};
+use log::{Level, LevelFilter, Metadata, Record};
+use serde_json::json;
 
-use log4rs::append::console::ConsoleAppender;
-//use log4rs::append::console::Target;
-use log4rs::config::{Appender, Config, Root}; 
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+	fn enabled(&self, metadata: &Metadata) -> bool {
+		// let target = metadata.target();
+		let lev = Level::Info;
+		metadata.level() <= lev
+	}
+
+	fn log(&self, record: &Record) {
+		println!(
+			"{} {} {} - {}",
+			Local::now().format("%Y-%m-%d %H:%M:%S"),
+			record.level(),
+			record.target(),
+			record.args()
+		);
+	}
+
+	fn flush(&self) {}
+}
+
+struct JsonLogger;
+
+impl log::Log for JsonLogger {
+	fn enabled(&self, metadata: &Metadata) -> bool {
+		// let target = metadata.target();
+		let lev = Level::Info;
+		metadata.level() <= lev
+	}
+
+	fn log(&self, record: &Record) {
+		let json = json!({
+			"@timestamp": format!("{}", Utc::now().format("%Y-%m-%dT%H:%M:%S%.3fZ")),
+			"ecs.version": "8.5",
+			"log.level": format!("{}", record.level()),
+			"log.logger": record.target(),
+			"message": record.args(),
+		});
+		println!("{}", json.to_string());
+	}
+
+	fn flush(&self) {}
+}
+
+#[cfg(debug_assertions)]
+static LOGGER: SimpleLogger = SimpleLogger;
+#[cfg(not(debug_assertions))]
+static LOGGER: JsonLogger = JsonLogger;
 
 pub fn init_logging() {
-	// let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
-	let stdout = ConsoleAppender::builder().build();
-
-	let config = Config::builder().appender(
-		Appender::builder().build("stdout", Box::new(stdout)),
-    ).build(
-        Root::builder()
-            .appender("stdout")
-            .build(log::LevelFilter::Info)
-    ).unwrap();
-
-	log4rs::init_config(config).unwrap();
+	log::set_logger(&LOGGER).unwrap();
+	log::set_max_level(LevelFilter::Info);
 }
 
