87f010b83236d6fac83cdf348985518fc45d0bab
Alessandro Pira <alex@alessandropira.org>
2024-09-05 11:38:45 +0200

    remove/add headers support

--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,12 @@ use std::{env,error::Error,collections::HashMap};
 use serde::Deserialize;
 use std::time::Duration;
 use std::net::{ToSocketAddrs, SocketAddr};
-use hyper::{Method,Uri,header::HeaderMap,StatusCode};
+use hyper::{Request,Method,Uri,header::{HeaderMap,HeaderName,HeaderValue},StatusCode};
 use regex::Regex;
 use log::{LevelFilter,info,warn};
 
+use crate::net::GatewayBody;
+use crate::service::ServiceError;
 use crate::c3po::HttpVersion;
 
 fn parse_array(v: &toml::Value) -> Option<Vec<String>> {
@@ -29,13 +31,29 @@ fn parse_array(v: &toml::Value) -> Option<Vec<String>> {
 		_ => None,
 	}
 }
-fn parse_map(v: &toml::Value) -> Option<HashMap<String,String>> {
-	let mut parsed = HashMap::<String,String>::new();
+fn parse_header_map(v: &toml::Value) -> Option<HeaderMap> {
+	let mut parsed = HeaderMap::new();
 
 	if let toml::Value::Table(t) = v {
 		for k in t.keys() {
 			if let Some(value) = t.get(k).and_then(|v| v.as_str()) {
-				parsed.insert(k.to_string(), value.to_string());
+				let hn = match HeaderName::from_bytes(k.as_bytes()) {
+					Ok(v) => v,
+					Err(_) => {
+						warn!("Invalid header name: {}", k);
+						continue;
+					},
+				};
+				let hv = match HeaderValue::from_bytes(value.as_bytes()) {
+					Ok(v) => v,
+					Err(_) => {
+						warn!("Invalid header value: {}", value);
+						continue;
+					},
+				};
+				if let Err(e) = parsed.try_append(hn,hv) {
+					warn!("Failed to add header {}: {:?}", k, e);
+				}
 			}
 		}
 	}
@@ -224,8 +242,8 @@ pub struct ConfigAction {
 	max_reply_log_size: Option<i64>,
 	ssl_mode: Option<SslMode>,
 	cafile: Option<PathBuf>,
-	remove_headers: Option<Vec<String>>,
-	add_headers: Option<HashMap<String,String>>,
+	remove_request_headers: Option<Vec<String>>,
+	add_request_headers: Option<HeaderMap>,
 }
 
 impl ConfigAction {
@@ -243,8 +261,8 @@ impl ConfigAction {
 				max_reply_log_size: t.get("max_reply_log_size").and_then(|v| v.as_integer()),
 				cafile: t.get("cafile").and_then(|v| v.as_str()).map(|v| Path::new(v).to_path_buf()),
 				ssl_mode: t.get("ssl_mode").and_then(|v| v.as_str()).map(|v| v.to_string().into()),
-				remove_headers: t.get("remove_headers").and_then(|v| parse_array(v)),
-				add_headers: t.get("add_headers").and_then(|v| parse_map(v)),
+				remove_request_headers: t.get("remove_request_headers").and_then(|v| parse_array(v)),
+				add_request_headers: t.get("add_request_headers").and_then(|v| parse_header_map(v)),
 			}),
 			_ => None,
 		}
@@ -262,6 +280,8 @@ impl ConfigAction {
 		self.max_reply_log_size = self.max_reply_log_size.take().or(other.max_reply_log_size);
 		self.cafile = self.cafile.take().or(other.cafile.clone());
 		self.ssl_mode = self.ssl_mode.take().or(other.ssl_mode);
+		self.remove_request_headers = self.remove_request_headers.take().or(other.remove_request_headers.clone());
+		self.add_request_headers = self.add_request_headers.take().or(other.add_request_headers.clone());
 	}
 
 	pub fn get_ssl_mode(&self) -> SslMode {
@@ -312,6 +332,26 @@ impl ConfigAction {
 
 	pub fn client_version(&self) -> HttpVersion {
 		self.http_client_version.unwrap_or(HttpVersion::H1)
+	}
+
+	pub fn adapt_request(&self, mut req: Request<GatewayBody>, corr_id: &str) -> Result<Request<GatewayBody>, ServiceError> {
+		let hdrs = req.headers_mut();
+
+		if let Some(hlist) = self.remove_request_headers.as_ref() {
+			for to_remove in hlist {
+				while hdrs.remove(to_remove).is_some() { }
+			}
+		}
+
+		if let Some(hlist) = self.add_request_headers.as_ref() {
+			for (k,v) in hlist {
+				if let Err(e) = hdrs.try_append(k.clone(),v.clone()) {
+					warn!("{}Failed to add header {}: {:?}", corr_id, k, e);
+				}
+			}
+		}
+
+		Ok(req)
 	}
 }
 
@@ -463,8 +503,8 @@ struct RawConfig {
 	max_reply_log_size: Option<i64>,
 	server_ssl_cert: Option<String>,
 	server_ssl_key: Option<String>,
-	remove_headers: Option<toml::Value>,
-	add_headers: Option<toml::Value>,
+	remove_request_headers: Option<toml::Value>,
+	add_request_headers: Option<toml::Value>,
 	filters: Option<toml::Table>,
 	actions: Option<toml::Table>,
 	rules: Option<toml::Table>,
@@ -491,8 +531,8 @@ impl RawConfig {
 			log_reply_body: None,
 			max_request_log_size: None,
 			max_reply_log_size: None,
-			remove_headers: None,
-			add_headers: None,
+			remove_request_headers: None,
+			add_request_headers: None,
 			filters: None,
 			actions: None,
 			rules: None,
@@ -539,8 +579,8 @@ impl RawConfig {
 		self.max_reply_log_size = self.max_reply_log_size.take().or(other.max_reply_log_size);
 		self.server_ssl_cert = self.server_ssl_cert.take().or(other.server_ssl_cert);
 		self.server_ssl_key = self.server_ssl_key.take().or(other.server_ssl_key);
-		self.remove_headers = self.remove_headers.take().or(other.remove_headers);
-		self.add_headers = self.add_headers.take().or(other.add_headers);
+		self.remove_request_headers = self.remove_request_headers.take().or(other.remove_request_headers);
+		self.add_request_headers = self.add_request_headers.take().or(other.add_request_headers);
 		self.filters = self.filters.take().or(other.filters);
 		self.actions = self.actions.take().or(other.actions);
 		self.rules = self.rules.take().or(other.rules);
@@ -667,8 +707,8 @@ impl Config {
 				max_request_log_size: raw_cfg.max_request_log_size,
 				log_reply_body: raw_cfg.log_reply_body,
 				max_reply_log_size: raw_cfg.max_reply_log_size,
-				remove_headers: raw_cfg.remove_headers.as_ref().and_then(|v| parse_array(v)),
-				add_headers: raw_cfg.add_headers.as_ref().and_then(|v| parse_map(v)),
+				remove_request_headers: raw_cfg.remove_request_headers.as_ref().and_then(|v| parse_array(v)),
+				add_request_headers: raw_cfg.add_request_headers.as_ref().and_then(|v| parse_header_map(v)),
 			},
 			bind: Self::parse_bind(&raw_cfg),
 			graceful_shutdown_timeout: Self::parse_graceful_shutdown_timeout(&raw_cfg),
--- src/service.rs
+++ src/service.rs
@@ -163,6 +163,7 @@ impl GatewayService {
 		});
 		Self::log_request(action, &req, corr_id, "->R");
 		let modified_request = action.client_version().adapt_request(cfg, action, req, corr_id)?;
+		let modified_request = action.adapt_request(modified_request, corr_id)?;
 		Self::log_request(action, &modified_request, corr_id, "R->");
 		Ok(modified_request)
 	}
